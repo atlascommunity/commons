0782a043f21c79ea529240421d491e6b1ae46c63
Ivan Mashintsev <i.mashintsev@corp.mail.ru>
2021-02-25 00:37:12 +0300

    PluginProperties is Component

--- src/test/java/ru/mail/jira/plugins/commons/PluginPropertiesTest.java
+++ src/test/java/ru/mail/jira/plugins/commons/PluginPropertiesTest.java
@@ -15,6 +15,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class PluginPropertiesTest {
+    private static PluginProperties pluginProperties;
 
     @BeforeAll
     static void setup() {
@@ -22,49 +23,51 @@ public class PluginPropertiesTest {
         ComponentAccessor.initialiseWorker(componentAccessorWorker);
         ApplicationProperties applicationProperties = mock(ApplicationPropertiesImpl.class);
         when(applicationProperties.getString(anyString())).thenReturn(null);
+
+        pluginProperties = new PluginProperties(applicationProperties);
     }
 
     @Test
     void getProperty_AssertionSuccess() {
-        Optional<String> value = PluginProperties.getInstance().getString("pluginName");
+        Optional<String> value = pluginProperties.getString("pluginName");
         assertTrue(value.isPresent());
         assertEquals("${project.name}", value.get());
     }
 
     @Test
     void getIntProperty_AssertionSuccess() {
-        Optional<Integer> value = PluginProperties.getInstance().getInt("int");
+        Optional<Integer> value = pluginProperties.getInt("int");
         assertTrue(value.isPresent());
         assertEquals(123, value.get());
     }
 
     @Test
     void getDoubleProperty_AssertionSuccess() {
-        Optional<Double> value = PluginProperties.getInstance().getDouble("double");
+        Optional<Double> value = pluginProperties.getDouble("double");
         assertTrue(value.isPresent());
         assertEquals(123.45, value.get());
     }
 
     @Test
     void getBooleanProperty_AssertionSuccess() {
-        Optional<Boolean> value = PluginProperties.getInstance().getBoolean("boolean");
+        Optional<Boolean> value = pluginProperties.getBoolean("boolean");
         assertTrue(value.isPresent());
         assertTrue(value.get());
     }
 
     @Test
     void getNonExistProperty_AssertionEmptyOption() {
-        assertFalse(PluginProperties.getInstance().getString("nonExist").isPresent());
+        assertFalse(pluginProperties.getString("nonExist").isPresent());
     }
 
     @Test
     void getNullKeyProperty_AssertionEmptyOption() {
-        assertFalse(PluginProperties.getInstance().getString(null).isPresent());
+        assertFalse(pluginProperties.getString(null).isPresent());
     }
 
     @Test
     void getNullKeyDefaultProperty_AssertionEmptyOption() {
-        assertEquals("defaultValue", PluginProperties.getInstance().getString(null,"defaultValue"));
+        assertEquals("defaultValue", pluginProperties.getString(null,"defaultValue"));
     }
 
 }
--- src/test/java/ru/mail/jira/plugins/commons/SentryClientInitializerTest.java
+++ src/test/java/ru/mail/jira/plugins/commons/SentryClientInitializerTest.java
@@ -1,11 +1,26 @@
 package ru.mail.jira.plugins.commons;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.atlassian.jira.config.properties.ApplicationProperties;
+import com.atlassian.jira.config.properties.ApplicationPropertiesImpl;
+import com.atlassian.jira.security.JiraAuthenticationContext;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class SentryClientInitializerTest {
 
+  private PluginProperties pluginProperties;
+  private JiraAuthenticationContext jiraAuthenticationContext;
+
   @org.junit.jupiter.api.BeforeEach
   void setUp() {
+    jiraAuthenticationContext = mock(JiraAuthenticationContext.class);
+
+    ApplicationProperties applicationProperties = mock(ApplicationPropertiesImpl.class);
+    when(applicationProperties.getString(anyString())).thenReturn(null);
+
+    pluginProperties = new PluginProperties(applicationProperties);
   }
 
   @org.junit.jupiter.api.Test
@@ -15,7 +30,7 @@ class SentryClientInitializerTest {
 
   @org.junit.jupiter.api.Test
   void afterPropertiesSet() throws Exception {
-    SentryClientInitializer sentryClientInitializer = new SentryClientInitializer();
+    SentryClientInitializer sentryClientInitializer = new SentryClientInitializer(jiraAuthenticationContext, pluginProperties);
     sentryClientInitializer.afterPropertiesSet();
 
     SentryClient.capture("test");
